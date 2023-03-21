3e1ba73da3e3db53c1f63ba3c78b111288d2f2ee
durga <durga@pop-os.localdomain>
2023-03-21 15:45:12 +1030

    🎨 chore(.projenrc.js): update author and package name 🔧 chore(.projenrc.js): update package name to use hyphen instead of slash 🔧 chore(.projenrc.js): update author email address 🔧 chore(.projenrc.js): update repository URL to use Tech9 organization 🔧 chore(.projenrc.js): update package description and keywords

--- .projenrc.js
+++ .projenrc.js
@@ -1,13 +1,13 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'JetBridge',
+  author: 'Durgaprasad Budhwani Tech9',
   authorAddress: 'durga@tech9.com',
   cdkVersion: '2.62.3',
   defaultReleaseBranch: 'main',
-  name: 'cdk-nextjs-standalone',
+  name: 'tech9-cdk-nextjs-standalone',
   repositoryUrl: 'https://github.com/jetbridge/cdk-nextjs.git',
   authorOrganization: true,
-  packageName: '@tech9/cdk-nextjs-standalone',
+  packageName: '@tech9-cdk-nextjs-standalone',
   description: 'Deploy a NextJS app to AWS using CDK. Uses standalone build and output tracing.',
   keywords: ['nextjs', 'next', 'aws-cdk', 'aws', 'cdk', 'standalone', 'iac', 'infrastructure', 'cloud', 'serverless'],
   eslintOptions: {
